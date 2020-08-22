a3f015409cba02d2040bdd174fd339ac87bc6c64
Alieltahan <alyaltahan@gmail.com>
2020-08-22 19:37:00 +0300

    renamefb

--- src/component/ShareButtons.jsx
+++ src/component/ShareButtons.jsx
@@ -20,8 +20,8 @@ function ShareButtons({
   const quote = `"${qText}" \n-${qAuthor}`;
   const urlParam = "https://randomquotez.herokuapp.com/";
   return (
-    <div title="Share it on Fb" className="row">
-      <div id="share-buttons" className="col">
+    <div className="row">
+      <div title="Share it on Fb" id="share-buttons" className="col">
         <span className="button-padding">
           <FacebookShareButton url={urlParam} quote={quote}>
             <FacebookIcon size={33} round />
